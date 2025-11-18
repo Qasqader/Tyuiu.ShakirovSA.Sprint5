a641ab0af6aeea51d3c9a9d96651f69267c96bf5
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-18 20:49:10 +0300

    + task 0

--- Tyuiu.ShakirovSA.Sprint5.Task0.V20.Lib/DataService.cs
+++ Tyuiu.ShakirovSA.Sprint5.Task0.V20.Lib/DataService.cs
@@ -6,10 +6,15 @@ namespace Tyuiu.ShakirovSA.Sprint5.Task0.V20.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double res =Math.Round(2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2, 3);
-            File.WriteAllText(path, Convert.ToString(res));
-            return path;
+            
+        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+
+        double res = Math.Round(2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2, 3);
+
+        File.WriteAllText(path, res.ToString());
+        return path;
+            
         }
     }
 }
+
