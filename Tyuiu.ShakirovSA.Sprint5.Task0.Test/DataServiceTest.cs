43638c4dfef881058c1723c9d9b3a30869feffed
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-18 20:27:44 +0300

    Add project files.

---
+++ Tyuiu.ShakirovSA.Sprint5.Task0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShakirovSA.Sprint5.Task0.V20.Lib;
+namespace Tyuiu.ShakirovSA.Sprint5.Task0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(5);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+
+        }
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint5.Task0.V20/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.ShakirovSA.Sprint5.Task0.V20.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("X = " + 2);
+        Console.WriteLine("Исходный массив");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(2);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан");
+        Console.ReadKey();
+    }
+}
