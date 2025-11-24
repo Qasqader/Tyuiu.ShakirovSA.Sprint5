b5cf077b72549e12827a2caca2cc1e84c01c5dcc
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-24 15:04:23 +0300

    + Task3

---
+++ Tyuiu.ShakirovSA.Sprint5.Task3.V30.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShakirovSA.Sprint5.Task3.V30.Lib;
+namespace Tyuiu.ShakirovSA.Sprint5.Task3.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(3);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+
+        }
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint5.Task3.V30/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ShakirovSA.Sprint5.Task3.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 3;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("Исходный массив");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан");
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
