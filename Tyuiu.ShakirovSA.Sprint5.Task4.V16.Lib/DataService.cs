3dae259cc996877120e74f7f1cf4005d5f54befb
Qasqader <Tyuiu.ShakirovSA@outlook.com>
2025-11-24 16:28:00 +0300

    + Task4

---
+++ Tyuiu.ShakirovSA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShakirovSA.Sprint5.Task4.V16.Lib
+{
+    public class DataService : ISprint5Task4V16
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+            double x = Convert.ToDouble(str, CultureInfo.InvariantCulture);
+            double res = Math.Round(Math.Cos(x) + Math.Pow(x, 2) - ((2 * x) / 1.2), 3);
+            return res;
+        }
+        
+    }
+}
---
+++ Tyuiu.ShakirovSA.Sprint5.Task4.V16/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ShakirovSA.Sprint5.Task4.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask4V16.txt";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Ответ = " + res);
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
